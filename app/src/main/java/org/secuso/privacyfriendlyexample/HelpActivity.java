b05ee5594bd7d9d32036ef582800b738c7166a67
Alexander Karakuz <alexander.karakuz@gmail.com>
2016-11-23 10:08:38 -0800

    changed example app to latest release version 2.0

--- app/src/main/java/org/secuso/privacyfriendlyexample/HelpActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyexample/HelpActivity.java
@@ -1,50 +1,37 @@
 package org.secuso.privacyfriendlyexample;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import android.os.Bundle;
-import android.widget.ExpandableListView;
+import android.preference.PreferenceFragment;
+import android.view.View;
 
 /**
- * Class structure taken from tutorial at http://www.journaldev.com/9942/android-expandablelistview-example-tutorial
- * last access 27th October 2016
+ * Created by yonjuni on 17.06.16.
  */
-
 public class HelpActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_help);
-
-        ExpandableListAdapter expandableListAdapter;
-        HelpDataDump helpDataDump = new HelpDataDump(this);
-
-        ExpandableListView generalExpandableListView = (ExpandableListView) findViewById(R.id.generalExpandableListView);
-
-        HashMap<String, List<String>> expandableListDetail = helpDataDump.getDataGeneral();
-        List<String> expandableListTitleGeneral = new ArrayList<String>(expandableListDetail.keySet());
-        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitleGeneral, expandableListDetail);
-        generalExpandableListView.setAdapter(expandableListAdapter);
-
-        ExpandableListView featuresExpandableListView = (ExpandableListView) findViewById(R.id.featuresExpandableListView);
-        expandableListDetail = helpDataDump.getDataFeatures();
-        List<String> expandableListTitleFeatures = new ArrayList<String>(expandableListDetail.keySet());
-        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitleFeatures, expandableListDetail);
-        featuresExpandableListView.setAdapter(expandableListAdapter);
-
-        ExpandableListView privacyExpandableListView = (ExpandableListView) findViewById(R.id.privacyExpandableListView);
-        expandableListDetail = helpDataDump.getDataPrivacy();
-        List<String> expandableListTitlePrivacy = new ArrayList<String>(expandableListDetail.keySet());
-        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitlePrivacy, expandableListDetail);
-        privacyExpandableListView.setAdapter(expandableListAdapter);
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new HelpFragment()).commit();
 
         overridePendingTransition(0, 0);
     }
 
+    @Override
     protected int getNavigationDrawerID() {
         return R.id.nav_help;
+    }
+
+    public static class HelpFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.help);
+        }
     }
 
 }
