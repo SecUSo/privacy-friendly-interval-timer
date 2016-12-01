e64891e11fbee1521441d400a59b76fc41cd2c8a
Alexander Karakuz <alexander.karakuz@gmail.com>
2016-12-01 12:57:29 -0800

    Adjusted info on the About Page and fixed Links

--- app/src/main/java/org/secuso/privacyfriendlyexample/AboutActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyexample/AboutActivity.java
@@ -5,6 +5,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.text.method.LinkMovementMethod;
+
+
 
 /**
  * Created by yonjuni on 15.06.16.
@@ -27,6 +31,9 @@ public class AboutActivity extends AppCompatActivity {
         }
 
         overridePendingTransition(0, 0);
+
+        ((TextView) findViewById(R.id.secusoURL)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     //@Override
